eea679938997395ab0b023d5d9448ff06611c9bc
alec boldyshev <alec.bozo@gmail.com>
2012-12-17 15:38:58 +0400

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :timesheet do
+  name 'Timesheet plugin'
+  author 'Alec Boldyshev'
+  description 'Timesheet reports'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+#  author_url 'http://example.com/about'
+end
